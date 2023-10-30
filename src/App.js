b7002f60bc2afb74c2239919d096e8d5e50241eb
Lugia808 <naelsilva808@gmail.com>
2023-10-29 23:50:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<body>
+    <div className='container-fluid bg-dark h-100' id='bg'>
+      <nav className="navbar navbar-expand-lg navbar-primary w-100 bg-light p-4" id='navbar'>
+        <a className="navbar-brand" href="#">Banco de Livros</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Página Inicial</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
+  </body>
   );
 }
 
