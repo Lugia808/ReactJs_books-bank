d1f969b172fd2bd43cd8036279ffee673ace254f
Lugia808 <naelsilva808@gmail.com>
2023-11-04 10:05:57 -0300

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "../App.css";
 import { BiBookAdd } from "react-icons/bi";
+import { IoIosArrowBack } from "react-icons/io";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,7 +13,6 @@ function Home() {
   const [allBooks, setAllBooks] = useState([]);
 
   useEffect(() => {
-    // Buscar todos os livros ao montar o componente
     fetchAllBooks();
   }, []);
 
@@ -38,10 +38,10 @@ function Home() {
 
       toast.success(`${response.data.title} foi cadastrado com sucesso`, {
         position: "top-right",
-        autoClose: 3000, // Fecha o alerta automaticamente após 3 segundos
-      });
+        autoClose: 3000,
+      })
+      setFormData({ title: "" });
     } catch (error) {
-      // Lide com erros apropriadamente
       console.error(error);
       toast.error("Ocorreu um erro ao cadastrar o livro", {
         position: "top-right",
@@ -55,44 +55,61 @@ function Home() {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleBack = () => {
+    setAbrirAddBook(false);
+  };
+
+  const toggleAddBook = () => {
+    setAbrirAddBook(!abrirAddBook);
+  };
+
   return (
     <div className="container">
-    <div className="containerBooks">
-      <div className="containerBooksIN">
-        <div className="addContainer">
-          <a className="linkGroup" onClick={() => setAbrirAddBook(true)}>
-            <span>Adicionar Livro </span>
-            <BiBookAdd size={30} color="#008000" />
-          </a>
-        </div>
+      <div className="containerBooks">
+        <div className="containerBooksIN">
+          {abrirAddBook ? (
+            <div style={{display:"none"}}>
 
-        
+            </div>
+          ) : (
+            <div className="addContainer">
+              <a className="linkGroup" onClick={toggleAddBook}>
+                <span>Adicionar Livro </span>
+                <BiBookAdd size={30} color="#008000" />
+              </a>
+            </div>
+          )}
+          {abrirAddBook && (
 
-      
-      </div>
-      {abrirAddBook && (
-          <div className="addBook">
-            <h3 id="cadastrarBook">Cadastrar Livro</h3>
-
-            <form className="formzin" onSubmit={handleSubmit}>
-              <label>Nome do livro</label>
-              <input
-                type="text"
-                name="title"
-                id="searchB"
-                value={formData.title}
-                onChange={handleChange}
-                placeholder="Digite o título do livro"
-              />
-              <button id="ButtonS" type="submit">
-                Adicionar
+            
+            <div className="addBook">
+              <button className="backButton" id="ButtonS1" onClick={handleBack}>
+                <IoIosArrowBack size={30} color="#0f0f0" /> 
               </button>
-            </form>
-          </div>
-        )}
+
+              <div className="titleGP">
+                <h3 id="cadastrarBook">Cadastrar Livro</h3>
+                <h5 style={{ textAlign: "center" }}>Nome do livro</h5>
+              </div>
+              <form className="formzin" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="title"
+                  id="searchB"
+                  value={formData.title}
+                  onChange={handleChange}
+                  placeholder="Digite o título do livro"
+                />
+                <button id="ButtonS" type="submit">
+                  Adicionar
+                </button>
+              </form>
+            </div>
+          )}
+        </div>
+      </div>
+      <ToastContainer />
     </div>
-    <ToastContainer /> {/* Container para exibir os alertas */}
-  </div>
   );
 }
 
