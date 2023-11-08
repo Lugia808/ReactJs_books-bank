62727e95a35abffa7c60ab8e1839c09eec04b6dc
Lugia808 <naelsilva808@gmail.com>
2023-11-08 20:21:37 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,34 +1,77 @@
 import "./App.css";
-import React, { Component } from "react"; // Change from Components to Component
+import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
-import AddBooks from "./components/addBooks"
-import All_Books from "./components/All_Books"
-import Status from "./components/Status"
+import AddBooks from "./components/addBooks";
+import All_Books from "./components/All_Books";
+import Status from "./components/Status";
 import "./App.css";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiMenu } from "react-icons/fi";
 import { BiBookBookmark } from "react-icons/bi";
 
 class App extends Component {
-  // Change from "app" to "App"
+  // Add this inside your class component
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDropdownOpen: false,
+    };
+  }
+
+  toggleDropdown = () => {
+    this.setState((prevState) => ({
+      isDropdownOpen: !prevState.isDropdownOpen,
+    }));
+  };
+
+  toggleDropdown = () => {
+    this.setState((prevState) => ({
+      isDropdownOpen: !prevState.isDropdownOpen,
+    }));
+  };
+
   render() {
     return (
       <Router>
-
-
         <nav>
           <div className="navbar">
-            <div className="groupL">
-              <Link to={'/'} id="linkHome"><h2>Book's Bank</h2></Link>
-              <BiBookBookmark size={25} color="#fff" />
-            </div>
             <div className="navbarGroup">
-              <Link className="buttonNav1" to={"/lista_de_leitura"} >Lista de Leitura</Link>
-              <a href="/addbooks" ><button className="buttonNav inicio">Adicionar livros</button></a>
-
-              <Link className="buttonNav" to='/allbooks'>All Books</Link>
-              <input type="text" placeholder="Search Books" />
-              <FiSearch size={25} color="#fff" id="search" />
+              <div className="groupL">
+                <Link to={"/"} id="linkHome">
+                  <h2>Book's Bank</h2>
+                </Link>
+                <a href="/">
+                  <BiBookBookmark size={25} color="#fff" />
+                </a>
+              </div>
+              {/* Ícone para abrir o menu dropdown */}
+              <div className="dropdown-icon" onClick={this.toggleDropdown}>
+                <i className="fas fa-bars"></i>
+              </div>
+              {/* Menu suspenso para dispositivos móveis */}
+              <div
+                className={`mobile-dropdown ${
+                  this.state.isDropdownOpen ? "open" : ""
+                }`}
+              >
+                <Link className="buttonNav1" to={"/lista_de_leitura"}>
+                <button class="buttonNav inicio">Lista de Leitura</button>
+                </Link>
+                <Link className="buttonNav" to="/allbooks">
+                <button class="buttonNav inicio">All Books</button>
+                </Link>
+                <Link to="/addbooks">
+                  <button className="buttonNav inicio">Adicionar livros</button>
+                </Link>
+              </div>
+              <div id="divInp">
+                <input type="text" placeholder="Search Books" />
+                <FiSearch size={25} color="#fff" id="search" />
+                <button onClick={this.toggleDropdown}>
+                  <FiMenu size={25} />
+                </button>
+              </div>
             </div>
           </div>
         </nav>
